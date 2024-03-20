b50ba0bf4830c2718cf4a7afd94b75a8d39f2780
SoviljIvana <isovilj1@gmail.com>
2024-03-20 13:42:02 +0100

    feat: custom metrics added feature/custom-metrics

---
+++ MetricsDemo/ProductMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace MetricsDemo
+{
+    public class ProductMetrics
+    {
+        private readonly Counter<int> _soldProductsCount;
+
+        public ProductMetrics(IMeterFactory meterFactory)
+        {
+            var meterInstance = meterFactory.Create("MetricsDemo.ProductStore");
+            _soldProductsCount = meterInstance.CreateCounter<int>("metricsdemo.productstore.sold_products_count");
+        }
+
+        public void CountProductsSold(int quantity)
+        {
+            _soldProductsCount.Add(quantity);
+        }
+    }
+}
